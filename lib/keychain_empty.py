cf275be76326729e83ff85e502fc0ed61a4070dd
Joshua Warner <SilverTrumpet999+github@gmail.com>
2020-04-25 19:01:19 -0700

    Add example keychain file.

---
+++ lib/keychain_empty.py
@@ -0,0 +1,11 @@
+__all__ = ['MQTT_API_KEY',
+           'MQTT_WRITE_API_KEY',
+           'CHANNEL',
+           'wifi_ssid',
+           'wifi_pw']
+
+MQTT_API_KEY = ""
+MQTT_WRITE_API_KEY = ""
+CHANNEL = ""
+wifi_ssid = ''
+wifi_pw = ''
